62636d7437b704a23251645617289e5f334058a0
Rcuban <raulmose87@gmail.com>
2018-09-27 18:06:15 -0300

    dsjndkjkdjkdj

--- js/enviar.js
+++ js/enviar.js
@@ -1,14 +1,23 @@
-document.querySelector('submit').addEventListener('click', saveDatos);
-
-
-
-function saveDatos() {
-    let snombre = document.querySelector('#nombre').value,
-     sapellido = document.querySelector('#apellido').value,
-     shm = document.querySelector('#sexo').value,
-     sedad= document.querySelector('#edad').value,
-    ssexo= document.querySelector('#hm').value;
-
-    addOjecto(snombre, sapellido, shm, sedad, ssexo)
+function validacion() {
+    let poppup = document.getElementById("json");
+    poppup.classList.remove("hide");
+    let nombre, apellido_paterno, apellido_materno, edad, correo, sexo, futbol, Hobbies;
+    nombre = document.getElementById("nombre").value;
+    apellido_paternoe = document.getElementById("apellido_paterno").value;
+    apellido_materno = document.getElementById("apellido_materno").value;
+    edad = document.getElementById("edad").value;
+    correo = document.getElementById("correo").value;
+    sexo = document.getElementById("sexo").value;
     
+
+   // console.log(sexo);
+
+  if(nombre.length == 0){
+    document.getElementById("json").appendChild="falta nombre";
+  }else{
+    document.getElementById('json').innerHTML = "";
+    }
+
+
 }
+    
--- js/object.js
+++ js/object.js
@@ -1,13 +1,45 @@
-let extructuta = [];
-function addOjecto(nombre, apellido, hm, edad, hobbit) {
-    let newObject = {
-        nombre: nombre,
-        apellido: apellido,
-        hm: hm,
-        edad: edad,
-        hobbit: hobbit
-    };
-    console.log(newObject);
-    extructuta.push(newObject);
+let json = {
+    "nombre": "",
+    "apellido_paterno": "",
+    "apellido_materno": "",
+    "correo": "",
+    "sexo": "",
+    "edad": 0,
+    "hobbit": [],
+    "futbol": "",
 }
+
+function rescatarFormulario(){
+  
+    let poppup = document.getElementById("json");
+    poppup.classList.remove("hide");
+    json.nombre = document.getElementById("nombre").value.checked;
+    json.apellido_paterno = document.getElementById("apellido_paterno").value;
+    json.apellido_materno = document.getElementById("apellido_materno").value;
+    json.correo = document.getElementById("correo").value;
+    json.edad = document.getElementById("edad").value;
+    json.sexo = document.getElementById("sexo").value;
+    document.getElementsByName("Hobbies").forEach(function(elemnent){
+        json.hobbit.push( elemnent.checked);
+
+    });
+    json.futbol = document.querySelector("[name='futbol']:checked").value;
+
+    let arregloDeLetras["a","b""c""d""""""""""""""""]
+
+
+
+     //pre.innerHTML = JSON.stringify(json);
+     console.log(json.nombre);
+     if(json.nombre == null){
+         alert("el nombre es obligatorio");
+         return 0;
+     }
+
+
+
+}   
+   
+
+   
 
